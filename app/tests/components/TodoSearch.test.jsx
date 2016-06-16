8ec8cf5dbdbe5f8869d17e8a59c217ed1b2840f3
Lokesh Chandawar <chandawarlokesh@gmail.com>
2016-06-16 18:21:29 +0530

    TodoSearch tested

--- app/tests/components/TodoSearch.test.jsx
+++ app/tests/components/TodoSearch.test.jsx
@@ -12,4 +12,25 @@ describe('TodoSearch', () => {
     expect(TodoSearch).toExist(1);
   });
 
+ it("should called onSearch  with valid searchText",() =>{
+   var spy=expect.createSpy();
+   var searchText="Dog";
+   var todoSearch=TestUtils.renderIntoDocument(<TodoSearch onSearch={spy}/>);
+   var $el=$(ReactDOM.findDOMNode(todoSearch));
+
+   todoSearch.refs.searchText.value=searchText;
+   TestUtils.Simulate.change(todoSearch.refs.searchText);
+   expect(spy).toHaveBeenCalledWith(false,searchText);
+ });
+
+ it("should called onSearch  with valid showCompleted check",() =>{
+   var spy=expect.createSpy();
+   var showCompleted=true;
+   var todoSearch=TestUtils.renderIntoDocument(<TodoSearch onSearch={spy}/>);
+   var $el=$(ReactDOM.findDOMNode(todoSearch));
+
+   todoSearch.refs.showCompleted.checked=showCompleted;
+   TestUtils.Simulate.change(todoSearch.refs.showCompleted);
+   expect(spy).toHaveBeenCalledWith(showCompleted,"");
+ });
 });
