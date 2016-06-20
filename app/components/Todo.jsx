4855c8c2f36a5d7b651d3c2a292b9598e561cac8
Lokesh Chandawar <chandawarlokesh@gmail.com>
2016-06-20 15:44:16 +0530

    moment is added

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -1,13 +1,21 @@
 var React=require('react');
-
+var moment = require('moment');
 var Todo=React.createClass({
   render:function(){
-    var {id, text, completed}=this.props;
+    var {id, text, completed,createAt,completedAt}=this.props;
+    var message="";
+    if(completed){
+      message='Completed '+ moment.unix(completedAt).format('MMM Do YYYY @ h:mm a');  
+    }else{
+      message='Created '+ moment.unix(createAt).format('MMM Do YYYY @ h:mm a');
+    }
     return(
       <div>
           <input type="checkbox"  onChange={()=>{
               this.props.onToggle(id);
-            }} checked={completed}/>{text}
+            }} checked={completed}/>
+            {text}
+            <p>{message}</p>
       </div>
     );
   }
